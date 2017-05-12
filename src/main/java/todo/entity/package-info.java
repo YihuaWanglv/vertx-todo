fb5482bd6666d023b00418a7e491fbbe8cb142ac
yihuawanglv <wanglvyihua@gmail.com>
2017-05-12 15:25:09 +0800

    submit

---
+++ src/main/java/todo/entity/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Indicates that this module contains classes that need to be generated /
+ * processed.
+ */
+@ModuleGen(name = "vertx-blueprint-todo-entity", groupPackage = "todo.entity")
+package todo.entity;
+
+import io.vertx.codegen.annotations.ModuleGen;
---
+++ src/main/java/todo/service/TodoService.java
@@ -0,0 +1,24 @@
+package todo.service;
+
+import io.vertx.core.Future;
+import todo.entity.Todo;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TodoService {
+
+	Future<Boolean> initData(); // 初始化数据（或数据库）
+
+	Future<Boolean> insert(Todo todo);
+
+	Future<List<Todo>> getAll();
+
+	Future<Optional<Todo>> getCertain(String todoID);
+
+	Future<Todo> update(String todoId, Todo newTodo);
+
+	Future<Boolean> delete(String todoId);
+
+	Future<Boolean> deleteAll();
+}
